bc97563a781837ba0088cadcb6a1c3a3053368c2
Liam Wu <willonms@outlook.com>
2019-01-11 18:46:21 +1100

    first stage of the project finished

--- test_blog/blogapp/views.py
+++ test_blog/blogapp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from blogapp.models import Post
+from blogapp.forms import PostForm
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.published_date = timezone.now()
+            message.save()
+            return redirect('post_detail', pk=message.pk)
+    else:
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = PostForm(request.POST or None, instance=post)
+
+    if request.method == "POST":
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        return render(request, 'blog/post_edit.html', {'form': form})
